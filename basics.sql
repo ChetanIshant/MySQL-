9f5026d1261644688fddfd3fa8c4b74503030990
Chetan Urf Ishant <ishunara25@gmail.com>
2026-01-15 23:43:27 +0530

    basics.sql

--- basics.sql
+++ basics.sql
@@ -9,7 +9,7 @@ USE startersql; --to use the same database for further queries
 
 CREATE TABLE  default_table(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,       
     email VARCHAR(100) UNIQUE NOT NULL,
     gender ENUM('Male', 'Female', 'Other'),
     date_of_birth DATE,
